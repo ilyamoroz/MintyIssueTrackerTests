ad40e687aef4da0e3cc3c93c132cbb3fde2ad3b2
i_moroz <i_moroz@mifprojects.com>
2022-02-02 16:46:57 +0200

    add directors

---
+++ MintyIssueTrackerTests/MintyIssueTrackerTests/Directors/AuthenticationDirector.cs
@@ -0,0 +1,67 @@
+﻿using Bogus;
+using MintyIssueTrackerTests.Model;
+using RestSharp;
+using System.Net;
+using System.Threading.Tasks;
+using System;
+using Newtonsoft.Json;
+
+namespace MintyIssueTrackerTests.Directors
+{
+    public static class AuthenticationDirector
+    {
+        private static EndpointBuilder _endpointBuilder = new EndpointBuilder();
+        private static Faker _bogus = new Faker();
+
+        public static async Task<UserModel> CreateCredentials()
+        {
+            var body = new CreateUserModel()
+            {
+                username = _bogus.Random.String2(minLength: 5, maxLength: 16),
+                password = _bogus.Random.String2(minLength: 5, maxLength: 16),
+                firstname = _bogus.Name.FirstName(),
+                lastname = _bogus.Name.LastName()
+            };
+
+            var response = await RequestFactory
+                .RequestManager
+                .CreatePostRequest()
+                .SetApiEndpoint(_endpointBuilder.BuildRegistrationEndpoint())
+                .SetBody(body)
+                .AddQueryParameter("roleName", "admin")
+                .SendRequest();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return new UserModel()
+                {
+                    Username = body.username,
+                    Password = body.password
+                };
+            }
+            else
+            {
+                throw new ArgumentNullException("User Credentials");
+            }
+        }
+
+        public static async Task<string> GetToken(UserModel credentials)
+        {
+            var response = await RequestFactory
+                .RequestManager
+                .CreatePostRequest()
+                .SetApiEndpoint(_endpointBuilder.BuildTokenEndpoint())
+                .SetBody(credentials)
+                .SendRequest();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return JsonConvert.DeserializeObject<TokenModel>(response.Content).access_token;
+            }
+            else
+            {
+                throw new ArgumentNullException("Access token");
+            }
+        }
+    }
+}
---
+++ MintyIssueTrackerTests/MintyIssueTrackerTests/Directors/JobDirector.cs
@@ -0,0 +1,36 @@
+﻿using Bogus;
+using MintyIssueTrackerTests.ExtentionMethods;
+using MintyIssueTrackerTests.Model;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace MintyIssueTrackerTests.Directors
+{
+    public static class JobDirector
+    {
+        private static Faker _bogus = new Faker();
+        private static EndpointBuilder _endpointBuilder = new EndpointBuilder();
+        
+        public static async Task<long> GetJobId(string token)
+        {
+            var job = new JobModel
+            {
+                Name = _bogus.Name.JobTitle(),
+                Description = _bogus.Name.JobDescriptor(),
+                TimeSchedule = _bogus.Date.Future()
+            };
+
+            var response = await RequestFactory
+                .RequestManager
+                .CreatePostRequest()
+                .SetApiEndpoint(_endpointBuilder.BuildCreateJobEndpoint())
+                .SetToken(token)
+                .SetBody(job)
+                .SendRequest();
+
+            return JsonConvert.DeserializeObject<CreateJobResponseModel>(response.Content).jobId;
+            
+        }
+    }
+}
