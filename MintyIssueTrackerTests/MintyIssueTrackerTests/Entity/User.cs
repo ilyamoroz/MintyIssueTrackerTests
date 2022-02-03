1d6701133809484792d56ad2464e52d42b3c32ee
i_moroz <i_moroz@mifprojects.com>
2022-02-03 09:46:44 +0200

    add entity

---
+++ MintyIssueTrackerTests/MintyIssueTrackerTests/Entity/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MintyIssueTrackerTests.Entity
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Password { get; set; }
+
+        public long RoleId { get; set; }
+
+        public string Salt { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
