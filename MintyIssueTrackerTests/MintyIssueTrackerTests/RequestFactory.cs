9083278ea6c539c0c61d409fdfdf67ef0aa1480b
i_moroz <i_moroz@mifprojects.com>
2022-02-04 16:26:18 +0200

    add logger into request factory

--- MintyIssueTrackerTests/MintyIssueTrackerTests/RequestFactory.cs
+++ MintyIssueTrackerTests/MintyIssueTrackerTests/RequestFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using MintyIssueTrackerTests.Logger;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace MintyIssueTrackerTests
@@ -6,6 +8,7 @@ namespace MintyIssueTrackerTests
     public static class RequestFactory
     {
         public static RequestManager RequestManager { get; set; } = new RequestManager();
+        private static RequestLogger _logger = new RequestLogger();
         public static RestRequest SetApiEndpoint(this RestRequest request, RestClient client)
         {
             RequestManager.SetClient(client);
@@ -13,19 +16,23 @@ namespace MintyIssueTrackerTests
         }
         public static RestRequest SetBody(this RestRequest request,  object body)
         {
+            _logger.WriteToLog("Add body: " + JsonConvert.SerializeObject(body));
             request.AddBody(body);
             return request;
         }
         public static RestRequest SetToken(this RestRequest request, string token)
         {
+            _logger.WriteToLog("Add token");
             return request.AddHeader("Authorization", ("Bearer " + token));
         }
         public static RestRequest SetFormData(this RestRequest request)
         {
+            _logger.WriteToLog("Add Form-Data");
             return request.AddHeader("Content-Type", "multipart/form-data");
         }
         public static async Task<RestResponse> SendRequest(this RestRequest request)
         {
+            _logger.WriteToLog("Send request");
             return await RequestManager.GetResponse(request);
         }
     }
