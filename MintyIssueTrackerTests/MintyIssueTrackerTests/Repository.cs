bc8c270633f34f35565798ab8270f860ac8f35e0
i_moroz <i_moroz@mifprojects.com>
2022-02-09 11:21:50 +0200

    add connection string from user secrets

--- MintyIssueTrackerTests/MintyIssueTrackerTests/Repository.cs
+++ MintyIssueTrackerTests/MintyIssueTrackerTests/Repository.cs
@@ -2,14 +2,21 @@
 using System.Linq;
 using System.Data;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using MintyIssueTrackerTests.Tests;
 
 namespace MintyIssueTrackerTests
 {
     public static class Repository
     {
-        private const string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=MintyIssueTrackerCore;Trusted_Connection=True;MultipleActiveResultSets=true";
+        /// <summary>
+        /// Returns value from database
+        /// </summary>
         public static T GetByKey<T>(string key, string value)
         {
+            var _connectionString = new ConfigurationBuilder().AddUserSecrets<UserTest>()
+                .Build().GetSection("ConnectionString").Value;
+
             using (IDbConnection db = new SqlConnection(_connectionString))
             {
                 return db.Query<T>($"SELECT * FROM {typeof(T).Name}s WHERE {key}='{value}'").SingleOrDefault();
