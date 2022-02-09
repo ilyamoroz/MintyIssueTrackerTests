185fdbf31fc7a4e598d0ffe5be3ebca41ce8a3bb
i_moroz <i_moroz@mifprojects.com>
2022-02-09 11:21:25 +0200

    add sumarry

--- MintyIssueTrackerTests/MintyIssueTrackerTests/Logger/RequestLogger.cs
+++ MintyIssueTrackerTests/MintyIssueTrackerTests/Logger/RequestLogger.cs
@@ -5,12 +5,18 @@ namespace MintyIssueTrackerTests.Logger
     public class RequestLogger : LoggerHandler
     {
         private static ILogger logger = LoggerConfig.Configure();
+
         public RequestLogger() : base(CreateLog){ }
+
+
         private static void CreateLog(string message)
         {
             logger.Information(message);
         }
 
+        /// <summary>
+        /// Write log
+        /// </summary>
         public void WriteToLog(string message)
         {
             OnLog(message);
